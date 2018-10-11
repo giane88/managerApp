95c797792ceb379f8b42ecb0ded138d4769b0cb8
matteo.gianello <matteo.gianello@creactives.com>
2018-10-11 13:53:27 +0200

    Inizialize spring boot project

---
+++ src/main/java/com/giane/managerapp/ManagerappApplication.java
@@ -0,0 +1,12 @@
+package com.giane.managerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ManagerappApplication.class, args);
+  }
+}
---
+++ src/test/java/com/giane/managerapp/ManagerappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.giane.managerapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManagerappApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
